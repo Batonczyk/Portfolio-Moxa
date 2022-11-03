40b86f6882e9663eea5c2492f2e41dc0307c3e06
Batonczyk <dxfanat@gmail.com>
2022-11-03 15:39:52 +0100

    Portfolio moxa

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $('.header-burger').click(function () {
+    $('.header__menu').toggleClass('open-menu');
+    $('.bg-photo').toggleClass('bg-photo--active');
+  });
+
+});
